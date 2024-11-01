0d39569a393bfa4fdac5a2d6f80cfc960256c94c
IlhamBudimansyah <165880852+IlhamBudimansyah@users.noreply.github.com>
2024-11-01 19:22:04 +0700

    Add files via upload

--- app/models/NilaiUjianModel.php
+++ app/models/NilaiUjianModel.php
@@ -1,30 +1,35 @@
 <?php
 
-require_once __DIR__ .'../../config/database.php';
-class nilaiUjian {
+require_once __DIR__ . '../../config/database.php';
+class nilaiUjian
+{
     private $db;
-    
 
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->db = new Database();
         $this->db->connect(); // Menghubungkan ke database
     }
 
-    public function getAllNilai() {
+    public function getAllNilai()
+    {
         $query = "SELECT * FROM data_nilai_ujian";
         $this->db->query($query);
         return $this->db->resultSet();
     }
 
-    public function getById($id) {
+    public function getById($id)
+    {
         $query = "SELECT * FROM data_nilai_ujian WHERE id_nilai = :id_nilai";
         $this->db->query($query);
         $this->db->bind(':id_nilai', $id);
         return $this->db->single();
     }
 
-    public function create($data) {
-        $query = "INSERT INTO data_nilai_ujian (id_matakuliah, nilai_matkul, nim) VALUES (:id_matakuliah, :nilai_matkul, :nim)";
+    public function create($data)
+    {
+        $query = "INSERT INTO data_nilai_ujian (nilai_matkul, id_matakuliah, nim) VALUES (:nilai_matkul, :id_matakuliah, :nim)";
         $this->db->query($query);
         $this->db->bind(':nilai_matkul', $data['nilai_matkul']);
         $this->db->bind(':id_matakuliah', $data['id_matakuliah']);
@@ -32,21 +37,20 @@ class nilaiUjian {
         return $this->db->execute();
     }
 
-    public function update($data) {
-        $query = "UPDATE data_nilai_ujian SET id_matakuliah = :id_matakuliah, nilai_matkul = :nilai_matkul, nim = :nim WHERE id_nilai = :id_nilai";
+    public function update($data)
+    {
+        $query = "UPDATE data_nilai_ujian SET nilai_matkul = :nilai_matkul WHERE id_nilai = :id_nilai";
         $this->db->query($query);
         $this->db->bind(':id_nilai', $data['id_nilai']);
-        $this->db->bind(':id_matakuliah', $data['id_matakuliah']);
         $this->db->bind(':nilai_matkul', $data['nilai_matkul']);
-        $this->db->bind(':nim', $data['nim']);
         return $this->db->execute();
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $query = "DELETE FROM data_nilai_ujian WHERE id_nilai = :id";
         $this->db->query($query);
         $this->db->bind(':id', $id);
         return $this->db->execute();
     }
 }
-?>
